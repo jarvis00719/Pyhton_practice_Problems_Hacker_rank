b00f71e3728a55ab104a99ae4f4848729dd8f287
jarvis00719 <guliaharsh146@gmail.com>
2026-04-21 19:50:59 +0530

    Refactor breaking_records function and add data visualization

--- Records_broken_func+data_visualzation.py
+++ Records_broken_func+data_visualzation.py
@@ -19,25 +19,42 @@ def breaking_records(score):
         elif s < min_score:
             min_score = s
             lowest_record_break +=1
-
+        
 
     return max_record_break, lowest_record_break
     
     
 #Input Statements!!
-games = int(input())
+no_of_games = int(input())
 scores = list(map(int, input().split()))
 
 max_record_break, lowest_record_break = breaking_records(scores)
 print(max_record_break, lowest_record_break)
 
 
-# Data Visualization -- Records Broken !!
-plt.plot(games, scores, marker='o', label='Scores')
-plt.xlabel("Game")
-plt.ylabel("Points")
-plt.title("Breaking Records Visualization")
+#Using games indicies for X-axis !!
+games = list(range(1, no_of_games+1))
 
 
+#Data Visualization !!
+
+plt.plot(games, scores, marker='o', label ='scores')
+plt.xlabel("Games")
+plt.ylabel("Scores")
+plt.title("Breaking Records")
+
+#Highlighting Min/Max breaking points !!
+
+max_score, min_score = scores[0], scores[0]
+
+for i, s in enumerate(scores):
+    
+    if s > max_score:
+        plt.scatter(games[i], s, color='green', label='New Max' if i==0 else "")
+        max_score = s
+    
+    elif s < min_score:
+        plt.scatter(games[i], s, color='red', label='New Min' if i==0 else "")
+        min_score = s
 plt.legend()
 plt.show()
